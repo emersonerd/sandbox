ffc3fd0b37d1c65c99f5638018a67b8d25504b03
Kimberly Emerson <emersonerd@outlook.com>
2018-10-19 06:56:15 -0500

    Add files via upload

---
+++ automate-iis.ps1
@@ -0,0 +1,4 @@
+﻿Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
+New-NetFirewallRule -DisplayName "Allow Inbound Port 80" -Direction Inbound -LocalPort 80 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName "Allow Outbound Port 80" -Direction Outbound -LocalPort 80 -Protocol TCP -Action Allow
